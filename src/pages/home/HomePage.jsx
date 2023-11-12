06969553e44d0f38e163eae2fa9b6cff5791815b
Thomas Brockington <xtombrock1989@gmail.com>
2023-11-12 18:10:53 +0000

    save

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -13,10 +13,36 @@ function HomePage() {
     setActiveNav('/');
   }, []);
 
-  const navigateToPage = (event) => {
-    const { id } = event.target;
-    setActiveNav(id);
-    navigate(`${id}`);
+  useEffect(() => {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId: 'YOUR_CLIENT_ID',
+        scope: 'https://www.googleapis.com/auth/drive.readonly',
+      });
+
+      window.gapi.client.load('drive', 'v3', () => {
+        // Authenticate the user here
+      });
+    });
+  }, []);
+
+  const downloadFile = async (fileId) => {
+    try {
+      const response = await window.gapi.client.drive.files.get({
+        fileId: fileId,
+        alt: 'media',
+      });
+
+      const fileData = response.result;
+
+      // Handle the downloaded file data, e.g., save it to your local machine
+    } catch (error) {
+      console.error('Error downloading file:', error);
+    }
+  };
+
+  const downloadBuildApi = () => {
+    console.log('downloadBuildApi');
   };
 
   return (
@@ -30,24 +56,24 @@ function HomePage() {
               <div>
                 <h2 className='text-xl font-semibold'>Welcome To</h2>
                 <h1 className='text-3xl font-poppins font-bold'>
-                  POLYGON CUTTER
+                  Google Download
                 </h1>
               </div>
-              <div className='mt-4 grid'>
-                <p>
-                  <span className='text-hyperlink-blue font-semibold'>
-                    <Link to='/sign-up'>Sign up</Link>
-                  </span>{' '}
-                  today and draw, <br /> plot or program the exact shape your
-                  project requires. <br /> Save your results or download the
-                  code <br /> straight to your cutting device.
-                </p>
-              </div>
+              <div className='mt-4 grid'></div>
             </article>
             {/* cta */}
             <div className='mt-6'>
-              <button id='/design' onClick={navigateToPage} className='px-4 py-2 bg-white outline-black outline outline-2 active:scale-95 no__highlights hover:bg-yellow-100 rounded-xl'>
-                Try Now For Free
+              <button
+                id='/design'
+                onClick={downloadBuildApi}
+                className='px-4 py-2 bg-white outline-black outline outline-2 active:scale-95 no__highlights hover:bg-yellow-100 rounded-xl'
+              >
+                Download Build
+              </button>
+            </div>
+            <div>
+              <button onClick={() => downloadFile('YOUR_FILE_ID')}>
+                Download File
               </button>
             </div>
           </div>
