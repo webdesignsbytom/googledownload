d1fea7d8825deb178e2eb880a85ef3f15b6756d7
Thomas Brockington <xtombrock1989@gmail.com>
2023-11-14 18:38:51 +0000

    ready for download code

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -13,36 +13,8 @@ function HomePage() {
     setActiveNav('/');
   }, []);
 
-  useEffect(() => {
-    window.gapi.load('client:auth2', () => {
-      window.gapi.client.init({
-        clientId: 'YOUR_CLIENT_ID',
-        scope: 'https://www.googleapis.com/auth/drive.readonly',
-      });
-
-      window.gapi.client.load('drive', 'v3', () => {
-        // Authenticate the user here
-      });
-    });
-  }, []);
-
-  const downloadFile = async (fileId) => {
-    try {
-      const response = await window.gapi.client.drive.files.get({
-        fileId: fileId,
-        alt: 'media',
-      });
-
-      const fileData = response.result;
-
-      // Handle the downloaded file data, e.g., save it to your local machine
-    } catch (error) {
-      console.error('Error downloading file:', error);
-    }
-  };
-
-  const downloadBuildApi = () => {
-    console.log('downloadBuildApi');
+  const downloadFromGoogleDrive = () => {
+    console.log('downloadFromGoogleDrive');
   };
 
   return (
@@ -54,7 +26,6 @@ function HomePage() {
           <div className='grid text-center outline outline-2 outline-black rounded-xl bg-yellow-400 px-6 py-8'>
             <article className=''>
               <div>
-                <h2 className='text-xl font-semibold'>Welcome To</h2>
                 <h1 className='text-3xl font-poppins font-bold'>
                   Google Download
                 </h1>
@@ -65,15 +36,10 @@ function HomePage() {
             <div className='mt-6'>
               <button
                 id='/design'
-                onClick={downloadBuildApi}
+                onClick={downloadFromGoogleDrive}
                 className='px-4 py-2 bg-white outline-black outline outline-2 active:scale-95 no__highlights hover:bg-yellow-100 rounded-xl'
               >
                 Download Build
-              </button>
-            </div>
-            <div>
-              <button onClick={() => downloadFile('YOUR_FILE_ID')}>
-                Download File
               </button>
             </div>
           </div>
